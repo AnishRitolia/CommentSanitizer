725870fb3b8e5557cb749a0ba4b7cf4997e93ffe
Anish Ritolia <83706591+AnishRitolia@users.noreply.github.com>
2024-06-09 17:38:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
 import pandas as pd
-import matplotlib.pyplot as plt
 import pandas_datareader as data
 import streamlit as st
 import datetime as dt
