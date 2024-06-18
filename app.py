4b33fb823d542fe1907077b6a6f2e2f923ce687f
Anish Ritolia <83706591+AnishRitolia@users.noreply.github.com>
2024-06-18 21:28:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -94,7 +94,7 @@ if selected == "Home":
     Navigate through the sections using the menu above to explore the different functionalities of Comment Sanitizer.
     """)
 
-    lottie_coading = load_lottiefile("app-lottie/home.json")
+    lottie_coading = load_lottiefile("img/home.json")
     st_lottie(
         lottie_coading,
         speed = 1,
@@ -193,7 +193,7 @@ if selected == "CSV File":
 if selected == "FAQ":
     st.header("Frequently Asked Questions")
 
-    ottie_coading = load_lottiefile("app-lottie/faq.json")
+    ottie_coading = load_lottiefile("img/faq.json")
     st_lottie(
         lottie_coading,
         speed=1,
